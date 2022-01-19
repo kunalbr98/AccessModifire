761355e84708a0e054401195477c04c1bfa78c56
kunalbr98 <97838164+kunalbr98@users.noreply.github.com>
2022-01-19 14:24:47 +0530

    Add files via upload
    #AccessModifire
    Phase-1 Practice project
    Question-Write a program to demonstrate how and when access modifiers are used.

---
+++ proAccessModifier.java
@@ -0,0 +1,14 @@
+package M1;
+
+
+import M.*;
+
+ class proAccessModifier3 extends proAccessModifier {
+
+	public static void main(String[] args) {
+		proAccessModifier3 obj = new proAccessModifier3 ();   
+	       obj.display();  
+	}
+
+}
+
---
+++ pubAccessModifire.java
@@ -0,0 +1,12 @@
+package M1;
+import M.*;
+
+ class pubAccessModifire1  {
+
+	public static void main(String[] args) {
+		
+		pubAccessModifire obj = new pubAccessModifire(); 
+        obj.display();  
+		
+	}
+}
